9209591164f3404340dcd2fd4482db543cf8b3a6
damola adebayo <adebayo10k@gmail.com>
2022-07-30 19:33:39 +0100

    update usage function

--- includes/helper.inc.sh
+++ includes/helper.inc.sh
@@ -39,8 +39,14 @@ function usage () {
 	cat <<_EOF	
 Usage:	$command_basename [dev|help]
 
-Checks public web page for latest product information.
+Checks a public web page for latest product information.
 Optionally downloads, verifies and installs latest package.
+
+Be aware that this program is an interesting workaround, but 
+relies on a public data source that is not guaranteed to persist.
+It should therefore not really be used outside of hobby and 
+development environments.
+Read the source, extend and test if you wish.
 
 dev	Run program in development only mode.
 
